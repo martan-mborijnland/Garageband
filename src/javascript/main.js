e7247ee5002fd474cc6821f7e76e45401e4cf914
martan <martan-van-verseveld@users.noreply.github.com>
2024-09-17 12:55:00 +0200

    .

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,5 +1,17 @@
-class API {
-    constructor() {
-        this.url = 'https://jsonplaceholder.typicode.com';
+function createRows(container, rows, columns) {
+    for (let i = 0; i < rows; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (let j = 0; j < columns; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
     }
 }
+
+window.addEventListener('load', () => {
+    const container = document.querySelector('.container');
+    createRows(container, 4, 16);
+});
